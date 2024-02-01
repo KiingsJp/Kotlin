2ceefeaf4116a246e93ef1b1ba309bf84c18649b
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-01 11:15:24 -0300

    Add files via upload

---
+++ Pair&Triple.kt
@@ -0,0 +1,11 @@
+package br.com.sankhya.doalti.aaTestes
+
+fun main() {
+
+    val pair = Pair(10,20)
+    println("${pair.first} - ${pair.second}")
+
+    val triple = Triple(10, 20, "texto")
+    println("${triple.first} - ${triple.second} - ${triple.third}")
+
+}
