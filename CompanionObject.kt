fce1dcdda568f405d03b9acccab835e30a9d23e7
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-28 21:50:27 -0300

    Update CompanionObject.kt

--- CompanionObject.kt
+++ CompanionObject.kt
@@ -9,17 +9,14 @@ class Avenida {
             /* --- Ações do sinaleiro --- */
             println("Respeite o sinaleiro")
         }
-
     }
 }
 
 fun main(){
-
     println("Velocidade maxima avenida: ${Avenida.VELOCIDADE_MAX_LEI}")
     println("Ponto perigoso da avenida: ${Avenida.SINALEIRO()}")
 
     // Instanciar uma classe Avenida vai poder dar um nome para ela
     val avenida = Avenida()
     avenida.nome = "João Pinheiro";
-
 }
