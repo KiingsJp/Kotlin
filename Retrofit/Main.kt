59eb76f0b865380d05bc20c9b1a8cf259244f9ab
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-03-12 16:05:50 -0300

    Update Main.kt

--- Retrofit/Main.kt
+++ Retrofit/Main.kt
@@ -2,6 +2,10 @@ import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Callback
 
+// NECESSÁRIO AS DEPENDÊNCIAS: 
+// "com.squareup.retrofit2:retrofit:2.9.0"
+// "com.squareup.retrofit2:converter-gson:2.9.0"
+
 fun main() {
     val retrofit = RetrofitHelper.retrofit.create(EnderecoAPI::class.java)
     val call = retrofit.buscarEndereco()
