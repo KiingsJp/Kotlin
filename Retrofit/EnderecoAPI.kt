5008359c55a47d7ff7c4433ee265d8c66e0748e3
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-03-21 17:13:36 -0300

    Update EnderecoAPI.kt

--- Retrofit/EnderecoAPI.kt
+++ Retrofit/EnderecoAPI.kt
@@ -3,6 +3,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface EnderecoAPI {
-    @GET("38400654/json")
-    fun buscarEndereco(): Call<Endereco>
+    @GET("{cep}/json/")
+    fun buscarEndereco( @Path("cep") cep: String ) : Call<Endereco>
 }
