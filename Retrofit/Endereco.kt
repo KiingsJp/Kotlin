2a8a608d9c2c78af08b113934da6cf0991390c81
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-03-21 17:14:25 -0300

    Update Endereco.kt

--- Retrofit/Endereco.kt
+++ Retrofit/Endereco.kt
@@ -1,6 +1,7 @@
 data class Endereco(
     val cep: String,
-    val logradouro: String,
+    @SerializedName("logradouro")
+    val rua: String,
     val complemento: String,
     val bairro: String,
     val localidade: String,
