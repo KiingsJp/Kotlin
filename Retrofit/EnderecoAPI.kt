7a715d6f9ff5116de3ae5943aeccf51938573d17
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-03-21 17:30:49 -0300

    Delete Retrofit directory

--- Retrofit/EnderecoAPI.kt
+++
@@ -1,8 +0,0 @@
-import retrofit2.Call
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface EnderecoAPI {
-    @GET("{cep}/json/")
-    fun buscarEndereco( @Path("cep") cep: String ) : Call<Endereco>
-}
