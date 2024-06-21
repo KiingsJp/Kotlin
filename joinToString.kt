b0505807abdad4abb2408459f58eb76eae72473b
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-06-21 16:20:59 -0300

    Update joinToString.kt

--- joinToString.kt
+++ joinToString.kt
@@ -2,8 +2,9 @@ val produtos: ArrayList<Produto> = ArrayList()
 produtos.add(criarProduto("1234", "Teste Produto 1", "1"))
 produtos.add(criarProduto("3213", "Teste Produto 2", "0"))
 produtos.add(criarProduto("3413", "Teste Produto 3", "1"))
+produtos.add(criarProduto("3245", "Teste Produto 4", "1"))
 
-val string = produtos.joinToString(separator = ", ", prefix = "(", postfix = ")") {
+val string = produtos.joinToString(separator = ", ", prefix = "(", postfix = ")", limit = 2, truncated = "limiteExcedido") {
     it.CODPROD
 }
 
